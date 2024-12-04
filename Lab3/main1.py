129ac82c490cbd3cbf736ded9681770467907755
Skr-EZ-het <kokorkin.aleksandr.2004@gmail.com>
2024-12-04 21:59:18 +0300

    Update main1.py

--- Lab3/main1.py
+++ Lab3/main1.py
@@ -1,18 +1,13 @@
-# Функция для поиска индекса товара
 def find_item_index(items, item):
     try:
-        # Поиск индекса первого вхождения
         return items.index(item)
     except ValueError:
-        # Если элемент не найден, возвращаем None
         return None
 
-# Список товаров
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
-# Поиск элементов в списке
 for find_item in ['банан', 'груша', 'персик']:
-    index_item = find_item_index(items_list, find_item)  # Вызов функции
+    index_item = find_item_index(items_list, find_item)
     if index_item is not None:
         print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
     else:
